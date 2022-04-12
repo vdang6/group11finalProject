a61d3527e8abb30cc50cd001e472ade5e5cfce11
vdang <vdang6@student.gsu.edu>
2022-04-12 01:22:57 -0400

    sprint 1

--- client/src/routes/RestaurantPage.jsx
+++ client/src/routes/RestaurantPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from "react-router-dom";
 
 const RestaurantPage = () => {
-  return (
-    <div> RestaurantPage </div>
-  )
-}
+  const {id} = useParams()
+  const  {selectedRestaurant, setrselectedRestaurant} = useContext(RestaurantsContext)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        console.log(response);
 
+        setSelectedRestaurant(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+};
 export default RestaurantPage
